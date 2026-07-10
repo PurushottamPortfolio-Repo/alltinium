5b1fa3505181e479630cc66872c852b1fe2ed0be
PurushottamPortfolio-Repo <purushottam.portfolio@gmail.com>
2026-07-10 13:57:29 +0000

    initial commit

---
+++ src/app/(website)/materials/page.tsx
@@ -0,0 +1,7 @@
+export default function Materials() {
+  return (
+    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <h1>Welcome to the Materials Page</h1>
+    </div>
+  );
+}
---
+++ src/app/not-found.tsx
@@ -0,0 +1,8 @@
+
+export default function Notfound() {
+  return (
+    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <h1>404 | Page not found</h1>
+    </div>
+  );
+}
