8f80ebf5dbd669baf6820c3508dc74a1079c1850
PurushottamPortfolio-Repo <purushottam.portfolio@gmail.com>
2026-07-10 16:46:00 +0000

    ESLint Configured

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import globals from "globals";
+import tseslint from "typescript-eslint";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
+  ...tseslint.configs.recommended,
   ...nextTs,
   // Override default ignores of eslint-config-next.
   globalIgnores([
@@ -13,6 +16,15 @@ const eslintConfig = defineConfig([
     "build/**",
     "next-env.d.ts",
   ]),
+    {
+    languageOptions: {
+      globals: globals.browser,
+    },
+    rules: {
+      "@typescript-eslint/no-unused-vars": "warn",
+      "no-console": "warn",
+    },
+  },
 ]);
 
 export default eslintConfig;
