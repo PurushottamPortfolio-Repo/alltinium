693e12a780967fb4e4caee5ed6e32d2c78481932
Purushottam Kumar <purushottam.portfolio@gmail.com>
2026-08-11 18:58:10 +0530

    bug-fix, mailer.ts

--- src/lib/email/mailer.ts
+++ src/lib/email/mailer.ts
@@ -16,11 +16,13 @@ export async function sendEmail({
   to,
   subject,
   html,
+  text,
   replyTo,
 }: {
   to: string;
   subject: string;
   html: string;
+  text: string;
   replyTo?: string;
 }) {
   return transporter.sendMail({
@@ -29,43 +31,6 @@ export async function sendEmail({
     subject,
     html,
     ...(replyTo ? { replyTo } : {}),
+    text,
   });
 }
-
-// import nodemailer from "nodemailer";
-
-// const smtpPort = Number(process.env.SMTP_PORT || 465);
-
-// if (!process.env.SMTP_HOST || !process.env.SMTP_USER || !process.env.SMTP_PASSWORD) {
-//   throw new Error("SMTP environment variables are missing");
-// }
-
-// const transporter = nodemailer.createTransport({
-//   host: process.env.SMTP_HOST,
-//   port: smtpPort,
-//   secure: smtpPort === 465,
-//   auth: {
-//     user: process.env.SMTP_USER,
-//     pass: process.env.SMTP_PASSWORD,
-//   },
-// });
-
-// export async function sendEmail({
-//   to,
-//   subject,
-//   html,
-//   text,
-// }: {
-//   to: string;
-//   subject: string;
-//   html: string;
-//   text?: string;
-// }) {
-//   return transporter.sendMail({
-//     from: process.env.SMTP_FROM || process.env.SMTP_USER,
-//     to,
-//     subject,
-//     html,
-//     text,
-//   });
-// }
