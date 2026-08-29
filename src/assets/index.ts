817a3e955b5f1ae08aa51f4c463952ab64a8a76c
Purushottam Kumar <purushottam.portfolio@gmail.com>
2026-08-29 21:35:28 +0530

    loading logo added

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -3,9 +3,11 @@ import { themeLogo } from "./logo";
 import { profileImages } from "./profiles";
 import { BlogCover } from "./blog";
 import { Services } from "./services";
+import fevi from "@/app/favicon.ico";
 
 export const assets = {
   heroBg,
+  logo: fevi,
   logo1: themeLogo,
   blogCover: BlogCover,
   profiles: profileImages,
